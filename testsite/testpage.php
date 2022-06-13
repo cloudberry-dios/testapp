f2357d34c798df38aa332ce1dbae204c3f41a431
cloudberry-dios <cloudberry-dios@protonmail.com>
2022-06-13 11:10:45 +0000

    de doorgevoerde wijziging

--- testsite/testpage.php
+++ testsite/testpage.php
@@ -8,7 +8,7 @@
     {
       if($_POST['txt_naam']!="" && $_POST['txt_password']!="")
       {
-        $url="http://10.3.11.161:5051/valideer/" . $_POST['txt_naam'] . "/" . $_POST['txt_password'];
+        $url="http://10.3.11.166:5051/valideer/" . $_POST['txt_naam'] . "/" . $_POST['txt_password'];
         $html1=file_get_contents($url);
       }
       else
@@ -18,7 +18,7 @@
     }
   if($focus==2)
    {
-     $url="http://10.3.11.161:5052/hoofdstad/" . $_POST['txt_land'];
+     $url="http://10.3.11.166:5052/hoofdstad/" . $_POST['txt_land'];
      $html2="hoofdstad van " . $_POST['txt_land'] . " is " . file_get_contents($url);
    }
 
@@ -33,6 +33,7 @@
 
 <body>
    <h1>Welkom op de testapplicatie voor DEVOPS, VERSIE-01</h1>
+   <h1>Dit is een wijziging</h1>
    <table style="width:100%"">
      <tr>
        <?php
